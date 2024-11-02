dc517cc2b0c18b113fdf0deb3925c02557454620
CJ Naujokas <cnaujoka@charlotte.edu>
2024-11-02 14:00:24 -0400

    fix: fixed imports on main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from models.db import db
 from models.models import Sheep
 
@@ -7,3 +7,11 @@ app = FastAPI()
 @app.get("/sheep/{id}", response_model=Sheep)
 def read_sheep(id: int):
     return db.get_sheep(id)
+
+@app.post("/sheep/", response_model=Sheep, status_code=201)
+def add_sheep(sheep: Sheep):
+    if sheep.id in db.data:
+        raise HTTPException(status_code=400, detail="Sheep already exists")
+    
+    db.data[sheep.id] = sheep
+    return sheep
--- tests/test_main.py
+++ tests/test_main.py
@@ -15,3 +15,19 @@ def test_read_sheep():
         "breed": "Gotland",
         "sex": "ewe"
     }
+    
+    
+def test_add_sheep():
+    response = client.post("/sheep/", json={
+        "id": 7,
+        "name": "Esther",
+        "breed": "Border Leicester",
+        "sex": "ewe"
+    })
+    assert response.status_code == 201
+    assert response.json() == {
+        "id": 7,
+        "name": "Esther",
+        "breed": "Border Leicester",
+        "sex": "ewe"
+    }
